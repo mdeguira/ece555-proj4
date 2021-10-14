d4249863a3bc96fe8d75fa73d2e5dd1e6f95b9da
kat-huckabay <khuckaba@vols.utk.edu>
2021-10-14 17:11:31 -0400

    Wrote ADC0_InitSWTriggerSeq3_Ch1

--- 2.DistanceADC-Part2/Objects/main.d
+++ 2.DistanceADC-Part2/Objects/main.d
@@ -1,5 +1,5 @@
 .\objects\main.o: Main.c
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: D:\CS\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: PLL.h
 .\objects\main.o: ADCCode.h
 .\objects\main.o: UART.h
--- 2.DistanceADC-Part2/Objects/uart.d
+++ 2.DistanceADC-Part2/Objects/uart.d
@@ -1,4 +1,4 @@
 .\objects\uart.o: UART.c
-.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: D:\CS\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\uart.o: UART.h
 .\objects\uart.o: tm4c123gh6pm.h
